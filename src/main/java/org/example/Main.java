3860462dedf627e9472cf0498fe8dd7aff53a60c
XuWeiHong <1913369727@qq.com>
2023-02-22 13:31:32 +0800

    first commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,12 @@
+package org.example;
+
+/**
+ * @author 许伟鸿
+ * @version 1.0
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("H");
+    }
+}
